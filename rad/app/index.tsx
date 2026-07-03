760fe99ea46ec196f6f7e1ec066b83a52470062e
Jude Rozario <juderzro08@gmail.com>
2026-07-03 00:08:24 -0400

    updated

--- rad/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Redirect } from 'expo-router'
-import { useAuth } from '@/hooks/useAuth'
-
-export default function Index() {
-    const { isAuthenticated } = useAuth()
-    return isAuthenticated
-        ? <Redirect href="/(app)/dashboard" />
-        : <Redirect href="/(auth)/login" />
-}
